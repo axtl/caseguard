0e11d1447c9c78c804c6a47f2f740180529be26c
alexandru <alex@hackd.net>
2010-01-15 11:23:29 -0500

    Scaffolding. OS detection works. Case check currently returns False and so control flow *always* goes back to the normal Mercurial add command

--- caseguard.py
+++ caseguard.py
@@ -1,21 +1,39 @@
+'''Guard against case-folding collisions on Mac OS X, by blocking an add that would cause a collision'''
 
-'''guard against case-folding collisions on Mac OS X, by blocking an add that would cause a collision'''
-
+import platform
+import re
 from mercurial.i18n import _
-from mercurial import hg, commands
+from mercurial import commands, extensions
 
-'''figure out either:
-    a) how to override the add command
-        in this case, we can verify there is no regexp match to any non-removed files.
-        figure out of hg knows about a files with a regexp/similar name
-            from hg log and friends, although i expect that, given a file, mercurial can pull its path in the repo and then we can use the filename as a base to request logs of regex-matched names
-        check the file status:
-            removed, in changeset, checked in and repo updated -> no problem
-            removed, not checked in -> case-fold, abort and request rename (print out old file name)
-            removed, not checked in -> works OK, it respects order of operations
-            other states?
-        if filesystem (re)move, then new file, then hg rm oldcase, file overwrite, not to worry (unless we want to caseguard the hg rm too, show warning when the user is about to remove an inexact match)
-    b) a pre-add hook
-        is 'add' (preadd) hookable?
-        is a hook enforceable? 
 '''
+TODO: a better error message... maybe.
+
+'''
+
+warning = _("""
+    I can't let you do that, Dave. 
+    
+""")
+
+def uisetup(ui):
+    if oscheck()==False:
+        ''' default return, this is not the extension they are looking for (non-Darwin) '''
+        return 
+    def reallyadd(orig, ui, repo, *pats, **opts):
+        if  casecollide(repo):
+            ui.warn(warning)
+        else:
+            return orig(ui, repo, *pats, **opts)
+
+    entry = extensions.wrapcommand(commands.table, 'add', reallyadd)
+
+'''Check the case of the given file against the repository. Return True on collisions'''
+''' TODO: fill in'''
+''' TODO: proper argument list'''
+def casecollide(repo):
+    return False
+    
+''' Check to make sure that we are running on OS X'''
+def oscheck():
+    osname = platform.system()
+    return re.match('[dD]arwin', osname)
