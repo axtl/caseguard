a6bdd0d70b121bee5a8f801b623730f06e3db2dd
alexandru <alex@hackd.net>
2010-01-17 14:56:09 -0500

    Fixed to adhere to PEP-8

--- caseguard.py
+++ caseguard.py
@@ -1,24 +1,26 @@
 # Copyright (C) 2010 - Alexandru Totolici.  All rights reserved.
+# http://hackd.net/wares/caseguard/
 #
 # This is a small extension for Mercurial (http://www.selenic.com/mercurial)
-# that prevents the addition of files to repositories when such additions 
+# that prevents the addition of files to repositories when such additions
 # will cause case-folding collisions on certain filesystems.
 #
-# To enable the "caseguard" extension globally, put these lines in your ~/.hgrc:
+# To enable the "caseguard" extension globally, put these lines in your
+# ~/.hgrc:
 #  [extensions]
 #  caseguard = /path/to/caseguard.py
 #
-# You may optionally add a section in the config file that specifies what options
-# you want to have always enabled:
+# You may optionally add a section in the config file that specifies what
+# options you want to have always enabled:
 #
 # [caseguard]
 # silent = true
 # override = true
 #
-# Please note that having override always enabled will revert the add command to
-# its normal behaviour, and caseguard will not block you from adding the files to
-# the repository. However, if silent = false, you will still get a listing of the
-# files that will cause problems.
+# Please note that having override always enabled will revert the add command
+# to its normal behaviour, and caseguard will not block you from adding the
+# files to the repository. However, if silent = false, you will still get a
+# listing of the files that will cause problems.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,8 +36,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-
-'''Guard against case-folding collisions by blocking the 'add' operation if it would cause a collision on the local repository'''
+'''Guard against case-folding collisions by blocking the 'add' operation if \
+it would cause a collision on the local repository'''
 
 import re
 from mercurial.i18n import _
@@ -43,10 +45,13 @@ from mercurial import commands, extensions, cmdutil
 
 warning = _("""    Case-collision danger\n""")
 
-def uisetup(ui):    
-    def reallyadd(orig, ui, repo, override=False, silent=False, *pats, **opts):
-        override |= ui.configbool('caseguard','override')
-        silent |= ui.configbool('caseguard','silent')
+
+def uisetup(ui):
+
+    def reallyadd(orig, ui, repo, override=False, silent=False, \
+        *pats, **opts):
+        override |= ui.configbool('caseguard', 'override')
+        silent |= ui.configbool('caseguard', 'silent')
         collision = casecollide(repo, silent, *pats, **opts)
         if not override and collision:
             ui.warn(warning)
@@ -54,10 +59,14 @@ def uisetup(ui):
             return orig(ui, repo, *pats, **opts)
 
     entry = extensions.wrapcommand(commands.table, 'add', reallyadd)
-    entry[1].append(('', 'override', False, _('add files regardless of possible case-collision problems')))
-    entry[1].append(('','silent', False,_('do not output a list of case-colliding files')))
+    entry[1].append(('', 'override', False, _('add files regardless of \
+        possible case-collision problems')))
+    entry[1].append(('', 'silent', False, _('do not output a list of \
+        case-colliding files')))
 
-    '''Check the case of the given file against the repository. Return True on collisions and print a list of problem-files'''
+    '''Check the case of the given file against the repository. Return \
+    True on collisions and print a list of problem-files'''
+
     def casecollide(repo, silent, *pats, **opts):
         colliding = False
         ctx = repo.changectx('tip')
@@ -74,7 +83,8 @@ def uisetup(ui):
                         if not fpat.search(pending):
                             colliding = True
                             if not silent:
-                                ui.status(_('EEE> %s and %s (tracked): case-collision danger\n' % (f, ctxmanit)))
+                                ui.status(_('EEE> %s and %s (tracked): \
+                                 case-collision danger\n' % (f, ctxmanit)))
                             else:
                                 return colliding
 
