53d3488b57e018e94a1eb15e1e845b37c87f4d32
alexandru <alex@hackd.net>
2010-01-22 12:46:51 -0800

    Review 22. use ui.note() for listing files; change output to match hg style

--- caseguard.py
+++ caseguard.py
@@ -14,12 +14,11 @@
 # options you want to have always enabled:
 #
 # [caseguard]
-# silent = true
 # override = true
 #
 # Please note that having override always enabled will revert the add command
 # to its normal behaviour, and caseguard will not block you from adding the
-# files to the repository. However, if silent = false, you will still get a
+# files to the repository. However, if you pass --verbose you will get a
 # listing of the files that will cause problems.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -36,23 +35,21 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-'''Guard against case-folding collisions by blocking the 'add' operation if \
+'''guard against case-folding collisions by blocking the 'add' operation if \
 it would cause a collision on the local repository'''
 
 import re
 from mercurial.i18n import _
 from mercurial import commands, extensions, cmdutil
 
-warning = _("""    Case-collision danger\n""")
+warning = _("""not adding anything: case-collision danger\n""")
 
 
 def uisetup(ui):
 
-    def reallyadd(orig, ui, repo, override=False, silent=False, \
-        *pats, **opts):
-        override |= ui.configbool('caseguard', 'override')
-        silent |= ui.configbool('caseguard', 'silent')
-        collision = casecollide(repo, silent, *pats, **opts)
+    def reallyadd(orig, ui, repo, override=False, *pats, **opts):
+        override = override or ui.configbool('caseguard', 'override')
+        collision = casecollide(ui, repo, *pats, **opts)
         if not override and collision:
             ui.warn(warning)
         else:
@@ -61,13 +58,11 @@ def uisetup(ui):
     entry = extensions.wrapcommand(commands.table, 'add', reallyadd)
     entry[1].append(('o', 'override', False, _('add files regardless of \
 possible case-collision problems')))
-    entry[1].append(('s', 'silent', False, _('do not output a list of \
-case-colliding files')))
 
     '''Check the case of the given file against the repository. Return \
-True on collisions and print a list of problem-files'''
+True on collisions and (optionally) print a list of problem-files.'''
 
-    def casecollide(repo, silent, *pats, **opts):
+    def casecollide(ui, repo, *pats, **opts):
         colliding = False
         ctx = repo.changectx('tip')
         ctxmanits = [item[0] for item in ctx.manifest().items()]
@@ -82,10 +77,7 @@ True on collisions and print a list of problem-files'''
                     if fpat.match(ctxmanit):
                         if not fpat.search(pending):
                             colliding = True
-                            if not silent:
-                                ui.status(_('EEE> %s and %s (tracked): \
-case-collision danger\n' % (f, ctxmanit)))
-                            else:
-                                return colliding
+                            ui.note(_('%s may cause a case-collision with \
+%s (already in repository)\n' % (f, ctxmanit)))
 
         return colliding
